99bb011cc41c378a26ea89eaa60fd59ce97f27ef
gmlee <gmlee@ntels.com>
2022-08-24 13:47:54 +0900

    add fetch & svgs

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,28 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 const lotto_round = "800";
+export async function getServerSideProps() {
+  const data = await (
+    await fetch(
+      `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${lotto_round}`
+    )
+  ).json();
 
-export default function Home() {
-	const [movies, setMovie] = useState([]);
-	useEffect(() => {
-		console.log("asdfasdf");
-		async () => {
-			try {
-				const data = await fetch(
-					`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${lotto_round}`
-				).json();
-				console.log(data);
-			} catch (err) {
-				console.log(err);
-			}
-		};
-	}, []);
-	return (
-		<div>
-			<Seo title="Home" />
-			<h1 className="active">Hello</h1>
-		</div>
-	);
+  return { props: { data } };
+}
+
+export default function Home({ data }) {
+  const [movies, setMovie] = useState([]);
+
+  useEffect(() => {
+    console.log("asdfasdf");
+    console.log(data);
+  }, []);
+
+  return (
+    <div>
+      <Seo title="Home" />
+      <h1 className="active">Hello</h1>
+    </div>
+  );
 }
