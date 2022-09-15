bb8db6dd2be2f33841c7643f1bd6b6152b042ff3
gmlee <gmlee@ntels.com>
2022-09-15 17:05:38 +0900

    update code

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   const lottohist = require("../../public/lotto_hist.json").rows;
   let last = lottohist.length - 1;
   let ltCur = lottohist[last];
+  let lt_hist = lottohist.slice(-9, -1);
   // useEffect(() => {
   // }, []);
 
@@ -23,6 +24,20 @@ export default function Home() {
         <img className="flex-1" src={"/plus.svg"} />
         <img className="flex-1" src={`/balls/${ltCur.ballbnus}.svg`} />
       </div>
+
+      {lt_hist.map((lt) => {
+        <div className="flex flex-row place-content-center space-x-2">
+          <img className="flex-1" src={`/balls/${lt.ball1}.svg`} />
+          <img className="flex-1" src={`/balls/${lt.ball2}.svg`} />
+          <img className="flex-1" src={`/balls/${lt.ball3}.svg`} />
+          <img className="flex-1" src={`/balls/${lt.ball4}.svg`} />
+          <img className="flex-1" src={`/balls/${lt.ball5}.svg`} />
+          <img className="flex-1" src={`/balls/${lt.ball6}.svg`} />
+          <img className="flex-1" src={"/plus.svg"} />
+          <img className="flex-1" src={`/balls/${lt.ballbnus}.svg`} />
+        </div>;
+      })}
+
       <style jsx>{`
         img {
           max-width: 50px;
