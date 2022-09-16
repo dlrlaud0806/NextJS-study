b57d650871f77daaf561729fbf93729629384e68
gmlee <gmlee@ntels.com>
2022-09-16 10:38:19 +0900

    add lotto hist

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home() {
-  const [movies, setMovie] = useState([]);
   const lottohist = require("../../public/lotto_hist.json").rows;
   let last = lottohist.length - 1;
   let ltCur = lottohist[last];
@@ -26,20 +25,23 @@ export default function Home() {
         <img className="flex-1" src={"/plus.svg"} />
         <img className="flex-1" src={`/balls/${ltCur.ballbnus}.svg`} />
       </div>
-
-      {lt_hist.map((lt) => {
-        <div className="flex flex-row place-content-center space-x-2">
-          <img className="flex-1" src={`/balls/${lt.ball1}.svg`} />
-          <img className="flex-1" src={`/balls/${lt.ball2}.svg`} />
-          <img className="flex-1" src={`/balls/${lt.ball3}.svg`} />
-          <img className="flex-1" src={`/balls/${lt.ball4}.svg`} />
-          <img className="flex-1" src={`/balls/${lt.ball5}.svg`} />
-          <img className="flex-1" src={`/balls/${lt.ball6}.svg`} />
-          <img className="flex-1" src={"/plus.svg"} />
-          <img className="flex-1" src={`/balls/${lt.ballbnus}.svg`} />
-        </div>;
-      })}
-
+      <h1 className="p-4 text-lg text-center font-medium">
+        당첨번호 내역 (~{ltCur.idx - 1}회)
+      </h1>
+      <div>
+        {lt_hist.map((lt) => (
+          <div className="flex flex-row place-content-center space-x-2">
+            <img className="flex-1" src={`/balls/${lt.ball1}.svg`} />
+            <img className="flex-1" src={`/balls/${lt.ball2}.svg`} />
+            <img className="flex-1" src={`/balls/${lt.ball3}.svg`} />
+            <img className="flex-1" src={`/balls/${lt.ball4}.svg`} />
+            <img className="flex-1" src={`/balls/${lt.ball5}.svg`} />
+            <img className="flex-1" src={`/balls/${lt.ball6}.svg`} />
+            <img className="flex-1" src={"/plus.svg"} />
+            <img className="flex-1" src={`/balls/${lt.ballbnus}.svg`} />
+          </div>
+        ))}
+      </div>
       <style jsx>{`
         img {
           max-width: 50px;
