11b1468f18a814bae4d5bb84a3df1df6935a59f6
gmlee <gmlee@ntels.com>
2022-09-16 15:58:04 +0900

    add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,45 @@
 import Seo from "../components/Seo";
 export default function About() {
-	return (
-		<div>
-			<Seo title="About" />
-			<h1>About</h1>
-		</div>
-	);
+  const lottohist = require("../../public/lotto_hist.json");
+
+  const lt_num_cnt = {};
+
+  for (let lt of lottohist) {
+    for (const key in lt) {
+      if (key.substring(0, 6) == "drwtNo" || key.substring(0, 6) == "bnusNo") {
+        lt_num_cnt[lt[key]] = lt_num_cnt[lt[key]]
+          ? (lt_num_cnt[lt[key]] += 1)
+          : 1;
+      }
+    }
+  }
+
+  const sortedball = Object.entries(lt_num_cnt).sort((a, b) =>
+    b[1] - a[1] == 0 ? a[0] - b[0] : b[1] - a[1]
+  );
+
+  return (
+    <div>
+      <Seo title="About" />
+      <h1 className="p-4 text-lg text-center font-medium">많이 당첨된 번호</h1>
+      <div>
+        {sortedball.map((ball, idx) => (
+          <div
+            key={idx}
+            className="flex flex-row place-content-center space-x-2"
+          >
+            <img className="flex-1" src={`/balls/${ball[0]}.svg`} />
+            <h1>{ball[1]}</h1>
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        img {
+          max-width: 50px;
+          max-height: 50px;
+          margin-bottom: 5px;
+        }
+      `}</style>
+    </div>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home() {
-  const lottohist = require("../../public/lotto_hist.json").rows;
+  const lottohist = require("../../public/lotto_hist.json");
   let last = lottohist.length - 1;
   let ltCur = lottohist[last];
-  let lt_hist = lottohist.slice(-9, -1);
+  const sorted_hist = Object.entries(lottohist.slice(-9, -1)).sort(
+    (a, b) => b["drwNoDate"] - a["drwNoDate"]
+  );
+  console.log(sorted_hist);
   // useEffect(() => {
   // }, []);
 
@@ -13,35 +16,38 @@ export default function Home() {
     <div>
       <Seo title="Home" />
       <h1 className="p-4 text-lg text-center font-medium">
-        {ltCur.idx}회 당첨번호 ({ltCur.lot_date})
+        {ltCur.drwNo}회 당첨번호 ({ltCur.drwNoDate})
       </h1>
       <div className="flex flex-row place-content-center space-x-2">
-        <img className="flex-1" src={`/balls/${ltCur.ball1}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.ball2}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.ball3}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.ball4}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.ball5}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.ball6}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.drwtNo1}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.drwtNo2}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.drwtNo3}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.drwtNo4}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.drwtNo5}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.drwtNo6}.svg`} />
         <img className="flex-1" src={"/plus.svg"} />
-        <img className="flex-1" src={`/balls/${ltCur.ballbnus}.svg`} />
+        <img className="flex-1" src={`/balls/${ltCur.bnusNo}.svg`} />
       </div>
       <h1 className="p-4 text-lg text-center font-medium">
-        당첨번호 내역 (~{ltCur.idx - 1}회)
+        당첨번호 내역 (~{ltCur.drwNo - 1}회)
       </h1>
       <div>
-        {lt_hist.map((lt, idx) => (
+        {sorted_hist.map((lt, idx) => (
           <div
             key={idx}
             className="flex flex-row place-content-center space-x-2"
           >
-            <img className="flex-1" src={`/balls/${lt.ball1}.svg`} />
-            <img className="flex-1" src={`/balls/${lt.ball2}.svg`} />
-            <img className="flex-1" src={`/balls/${lt.ball3}.svg`} />
-            <img className="flex-1" src={`/balls/${lt.ball4}.svg`} />
-            <img className="flex-1" src={`/balls/${lt.ball5}.svg`} />
-            <img className="flex-1" src={`/balls/${lt.ball6}.svg`} />
+            <h1 className="p-4 text-lg text-center font-medium">
+              {lt[1].drwNo - 1}
+            </h1>
+            <img className="flex-1" src={`/balls/${lt[1].drwtNo1}.svg`} />
+            <img className="flex-1" src={`/balls/${lt[1].drwtNo2}.svg`} />
+            <img className="flex-1" src={`/balls/${lt[1].drwtNo3}.svg`} />
+            <img className="flex-1" src={`/balls/${lt[1].drwtNo4}.svg`} />
+            <img className="flex-1" src={`/balls/${lt[1].drwtNo5}.svg`} />
+            <img className="flex-1" src={`/balls/${lt[1].drwtNo6}.svg`} />
             <img className="flex-1" src={"/plus.svg"} />
-            <img className="flex-1" src={`/balls/${lt.ballbnus}.svg`} />
+            <img className="flex-1" src={`/balls/${lt[1].bnusNo}.svg`} />
           </div>
         ))}
       </div>
