3c930c2cf8cd5c4ab018a424690343866cab5665
gmlee <gmlee@ntels.com>
2022-09-22 17:02:45 +0900

    modified css - add ball-img

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,54 +8,74 @@ export default function Home() {
   const sorted_hist = Object.entries(lottohist.slice(-9, -1)).sort(
     (a, b) => a["drwNo"] - b["drwNo"]
   );
-  console.log(sorted_hist);
   // useEffect(() => {
   // }, []);
 
   return (
-    <div>
+    <div className="max-w-full max-h-full mx-auto">
       <Seo title="Home" />
       <h1 className="p-4 text-lg text-center font-medium">
         {ltCur.drwNo}회 당첨번호 ({ltCur.drwNoDate})
       </h1>
-      <div className="flex flex-row place-content-center space-x-2">
-        <img className="flex-1" src={`/balls/${ltCur.drwtNo1}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.drwtNo2}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.drwtNo3}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.drwtNo4}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.drwtNo5}.svg`} />
-        <img className="flex-1" src={`/balls/${ltCur.drwtNo6}.svg`} />
-        <img className="flex-1" src={"/plus.svg"} />
-        <img className="flex-1" src={`/balls/${ltCur.bnusNo}.svg`} />
+      <div className="flex place-content-center space-x-3">
+        <h1 className="p-4 text-lg text-center font-medium">{ltCur.drwNo}</h1>
+        <img className="ball-img flex-1" src={`/balls/${ltCur.drwtNo1}.svg`} />
+        <img className="ball-img flex-1" src={`/balls/${ltCur.drwtNo2}.svg`} />
+        <img className="ball-img flex-1" src={`/balls/${ltCur.drwtNo3}.svg`} />
+        <img className="ball-img flex-1" src={`/balls/${ltCur.drwtNo4}.svg`} />
+        <img className="ball-img flex-1" src={`/balls/${ltCur.drwtNo5}.svg`} />
+        <img className="ball-img flex-1" src={`/balls/${ltCur.drwtNo6}.svg`} />
+        <img className="ball-img flex-1" src={"/plus.svg"} />
+        <img className="ball-img flex-1" src={`/balls/${ltCur.bnusNo}.svg`} />
       </div>
       <h1 className="p-4 text-lg text-center font-medium">
         당첨번호 내역 (~{ltCur.drwNo - 1}회)
       </h1>
-      <div>
-        {sorted_hist.map((lt, idx) => (
-          <div
-            key={idx}
-            className="flex flex-row place-content-center space-x-2"
-          >
-            <h1 className="p-4 text-lg text-center font-medium">
-              {lt[1].drwNo}
-            </h1>
-            <img className="flex-1" src={`/balls/${lt[1].drwtNo1}.svg`} />
-            <img className="flex-1" src={`/balls/${lt[1].drwtNo2}.svg`} />
-            <img className="flex-1" src={`/balls/${lt[1].drwtNo3}.svg`} />
-            <img className="flex-1" src={`/balls/${lt[1].drwtNo4}.svg`} />
-            <img className="flex-1" src={`/balls/${lt[1].drwtNo5}.svg`} />
-            <img className="flex-1" src={`/balls/${lt[1].drwtNo6}.svg`} />
-            <img className="flex-1" src={"/plus.svg"} />
-            <img className="flex-1" src={`/balls/${lt[1].bnusNo}.svg`} />
-          </div>
-        ))}
-      </div>
+      {sorted_hist.map((lt, idx) => (
+        <div key={idx} className="flex place-content-center space-x-3">
+          <h1 className="p-4 text-lg text-center font-medium">{lt[1].drwNo}</h1>
+          <img
+            className="ball-img flex-1"
+            src={`/balls/${lt[1].drwtNo1}.svg`}
+          />
+          <img
+            className="ball-img flex-1"
+            src={`/balls/${lt[1].drwtNo2}.svg`}
+          />
+          <img
+            className="ball-img flex-1"
+            src={`/balls/${lt[1].drwtNo3}.svg`}
+          />
+          <img
+            className="ball-img flex-1"
+            src={`/balls/${lt[1].drwtNo4}.svg`}
+          />
+          <img
+            className="ball-img flex-1"
+            src={`/balls/${lt[1].drwtNo5}.svg`}
+          />
+          <img
+            className="ball-img flex-1"
+            src={`/balls/${lt[1].drwtNo6}.svg`}
+          />
+          <img className="ball-img flex-1" src={"/plus.svg"} />
+          <img className="ball-img flex-1" src={`/balls/${lt[1].bnusNo}.svg`} />
+        </div>
+      ))}
       <style jsx>{`
+        div {
+          flex-flex-1: 1;
+        }
+
+        .ball-img {
+          max-width: 8%;
+          max-height: 8%;
+        }
+
         img {
           max-width: 50px;
           max-height: 50px;
-          margin-bottom: 5px;
+          margin: 5px;
         }
       `}</style>
     </div>
