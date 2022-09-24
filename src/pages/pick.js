a39c0a11623d4cee819843899b80cccfd1cf7fd7
gimyeong <dlrlaud0806@gmail.com>
2022-09-24 21:38:37 +0900

    modified key error

--- src/pages/pick.js
+++ src/pages/pick.js
@@ -8,20 +8,20 @@ export default function Pick() {
 			<h1 className="p-4 text-md text-center font-medium">포함 번호</h1>
 			<div className="max-w-md mx-auto flex flex-wrap place-content-center space-x-1">
 				{numbers.map((ball, idx) => (
-					<img src={`/balls/${ball}.svg`} />
+					<img key={idx} src={`/balls/${ball}.svg`} />
 				))}
 			</div>
 			<h1 className="p-4 text-md text-center font-medium">제외 번호</h1>
 			<div className="max-w-md mx-auto flex flex-wrap place-content-center space-x-1">
 				{numbers.map((ball, idx) => (
-					<img src={`/balls/${ball}.svg`} />
+					<img key={idx} src={`/balls/${ball}.svg`} />
 				))}
 			</div>
 			<div className="container">
 				<h1 className="text-md text-center"> [버튼] 추첨 시작! </h1>
 				<div className="max-w-md mx-auto flex flex-wrap place-content-center space-x-1"></div>
 				{pick.map((ball, idx) => (
-					<img src={`/balls/${ball}.svg`} />
+					<img key={idx} src={`/balls/${ball}.svg`} />
 				))}
 			</div>
 			<style jsx>{`
